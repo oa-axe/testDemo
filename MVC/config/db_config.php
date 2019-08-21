6593e63d0343cbffe5bca2efdffa8a37a9c6be3d
Maple-Leaf-blip <54311513+Maple-Leaf-blip@users.noreply.github.com>
2019-08-21 14:33:27 +0800

    1
    1

--- MVC/config/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php   
-// 链接数据库的基本配置信息
-const DBMS='mysql';
-const HOST='localhost';
-const DBNAME='db_oa';
-const DBUSER='root';
-const DBPW='';
-const CHARSET='utf8';
-// return ['host'=>HOST,'db_name'=>DBNAME]
